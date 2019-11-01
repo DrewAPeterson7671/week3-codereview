51e6fdad0f1db2eec5035a81c6b8f70bde7b6b44
Student <example@example.com>
2019-11-01 09:36:28 -0700

    Work on backend logic initial logic and structure.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
-
-
-
-
-
-
-
-
-
-
-
-
-
     var userNumber = $("input#userNumber").val();
-    $("#backendOutput").text(userNumber);
+    countDown(userNumber);
+    $("#backendOutput").text();
+
+
+
+
+
+    var numberStorage = [];
+
+    function countDown (inputNumber) {
+      for (var i = inputNumber; i < 0; i -= 1) {
+        console.log(inputNumber);
+        numberStorage.push(inputNumber - i);
+        return numberStorage;
+      }
+      console.log(numberStorage);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
